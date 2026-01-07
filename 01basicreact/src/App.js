54c3055fb2c147681e94d3f7d632deae20b07ca9
Priyanshu <singhpriyanshu608@gmail.com>
2026-01-07 22:05:02 +0530

    Initial commit: 3 React projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+<h1>hello world | Priyanshu Singh
+  Started learning react;
+  After having some practise of JS,CSS ;
+  heading toward to learn React library to maintain and make JS 
+  more advanced and easy to used in workflow :)
+</h1>
+  );
+}
+
+export default App;
